ffd98ef33271840379235d7a2c8c66a0a15cfa7d
NeonDragon1909 <nahil123@live.com>
2018-09-18 20:06:59 +0500

    dont trigger sepolicy neverallows #2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,7 +65,6 @@ TARGET_SPECIFIC_HEADER_PATH := device/huawei/hi6250/include
 
 # SELinux Policies
 BOARD_SEPOLICY_DIRS := device/huawei/hi6250/sepolicy
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Logging
 TWRP_INCLUDE_LOGCAT := true
